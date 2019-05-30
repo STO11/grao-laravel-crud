2f53d9c0ddb8a2790ecfd5023a518a59260a0097
Bredi Tecnologia <bredi@Mac-Pro-de-Bredi.local>
2019-05-30 09:29:44 -0300

    Versão mais estavel

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Grão CLI (Crud para Laravel)
 
-Um CLI para criação de arquivos básicos para laravel.
+Um CLI para criação de arquivos básicos para Laravel.
 
 # Usage (Install global) 
 
@@ -13,14 +13,38 @@ npm i -g grao-laravel-crud
 # Usage (CLI)
 ----------------------------------------------------
 
-> grao g table_name namespace
+Before running the command, the table in the database should exist.
+
+IMPORTANT: This module only uses MYSQL connection, reading the .env file.
+
+COMMAND:
+> grao g table_name namespace 
+or 
+> grao generate table_name namespace
 
 Where table_name is the name of the table
 
-Where namespace is the name of the Workspace / Namespace 
+Where namespace is the name of the Workspace / Namespace.
 
 ----------------------------------------------------
 
+# Folder grao-config (CLI)
+
+----------------------------------------------------
+
+If you want to change something in the next 'grud',
+the grao-config folder will be created in your directory with .ejs files for modifications,
+any modification to these files will affect the generation of the grud`s files in the application. 
+If you want to re-create the folder just delete it and the files will come in the default.
+
+Or you can use the command below in laravel project. 
+He had erased and recreated the folder:
+
+> grao reset
+or 
+> grao r
+
+----------------------------------------------------
 # License
 
 MIT - see LICENSE
---
+++ src/commands/reset.js
@@ -0,0 +1,108 @@
+
+module.exports = {
+    name: 'reset',
+    alias: ['r'],
+    run: async toolbox => {
+      const { print: { info, error, success } } = toolbox
+      const fs = require('fs');
+      const rmdirAsync = function(path, callback) {
+          fs.readdir(path, function(err, files) {
+              if(err) {
+                  // Pass the error on to callback
+                  callback(err, []);
+                  return;
+              }
+              var wait = files.length,
+                  count = 0,
+                  folderDone = function(err) {
+                  count++;
+                  // If we cleaned out all the files, continue
+                  if( count >= wait || err) {
+                      fs.rmdir(path,callback);
+                  }
+              };
+              // Empty directory to bail early
+              if(!wait) {
+                  folderDone();
+                  return;
+              }
+              // Remove one or more trailing slash to keep from doubling up
+              path = path.replace(/\/+$/,"");
+              files.forEach(function(file) {
+                  var curPath = path + "/" + file;
+                  fs.lstat(curPath, function(err, stats) {
+                      if( err ) {
+                          callback(err, []);
+                          return;
+                      }
+                      if( stats.isDirectory() ) {
+                          rmdirAsync(curPath, folderDone);
+                      } else {
+                          fs.unlink(curPath, folderDone);
+                      }
+                  });
+              });
+          });
+      };
+
+      info('Reseting config folder');
+
+      rmdirAsync('./grao-config', function() {
+        let dir = './grao-config';
+        if (!fs.existsSync(dir))
+        {
+            fs.mkdirSync(dir);
+            fs.readFile(__dirname+'/../templates/index.js.ejs', function(err, data) {
+                if (err) {
+                throw err;
+                }
+                fs.writeFile('./grao-config/index.js.ejs', data,function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                });
+            });
+            fs.readFile(__dirname+'/../templates/form.js.ejs', function(err, data) {
+                if (err) {
+                throw err;
+                }
+                fs.writeFile('./grao-config/form.js.ejs', data,function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                });
+            });
+
+            fs.readFile(__dirname+'/../templates/controller.js.ejs', function(err, data) {
+                if (err) {
+                throw err;
+                }
+                fs.writeFile('./grao-config/controller.js.ejs', data,function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                });
+            });
+
+            fs.readFile(__dirname+'/../templates/model.js.ejs', function(err, data) {
+                if (err) {
+                throw err;
+                }
+                fs.writeFile('./grao-config/model.js.ejs', data,function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                });
+            });
+            fs.writeFile('./grao-config/routes.php', '<?php '+toolbox.filesystem.eol,function(err, data) {
+                if (err) {
+                throw err;
+                }
+            });
+        }
+        success('Successfully config reset');
+        });
+
+    }
+}
+  
